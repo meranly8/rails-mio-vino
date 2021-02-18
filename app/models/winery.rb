3964ffc3fb4a2df8c7b7b9c2e8659373a107fd72
Michelle Ranly <meranly8@gmail.com>
2021-02-17 21:51:10 -0600

    Added Winery associations to :wines and :users

--- app/models/winery.rb
+++ app/models/winery.rb
@@ -2,4 +2,7 @@ class Winery < ApplicationRecord
     validates :name, presence: true
     validates :city, presence: true
     validates :country, presence: true
+
+    has_many :wines
+    has_many :users, through: :wines
 end
