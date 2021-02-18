cfe3d42e82819249c8b105341477cbc686b9a6dd
Michelle Ranly <meranly8@gmail.com>
2021-02-17 21:47:44 -0600

    Added Wine associations to :user and :winery

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -2,4 +2,7 @@ class Wine < ApplicationRecord
     validates :name, presence: true
     validates :wine_type, presence: true
     validates :year, presence: true
+
+    belongs_to :user
+    belongs_to :winery
 end
