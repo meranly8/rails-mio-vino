6611960236d04bd52fcfc9dca99faba7edf000a3
Michelle Ranly <meranly8@gmail.com>
2021-02-17 21:36:44 -0600

    Added User associations to :wines and :wineries

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
     validates :name, presence: true
     validates :email, presence: true
     validates :email, uniqueness: true
+
+    has_many :wines
+    has_many :wineries, through: :wines
 end
