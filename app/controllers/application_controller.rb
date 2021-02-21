6b0b08372dec30377ca2c646eeadfb84d46591c9
Michelle Ranly <meranly8@gmail.com>
2021-02-21 11:13:02 -0600

    Defined #current_user, #logged_in?, #enforce_login app helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+    def current_user
+        @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+    def enforce_login
+        return redirect_to login_path unless logged_in?
+    end
 end
