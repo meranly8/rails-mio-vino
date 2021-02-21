4375b42dd546fd88922a88e9c714c5e2bf2d0bfe
Michelle Ranly <meranly8@gmail.com>
2021-02-21 14:25:57 -0600

    Added Winery class methods to .order_by_established, .order_by_name, .order_by_region

--- app/models/winery.rb
+++ app/models/winery.rb
@@ -5,4 +5,16 @@ class Winery < ApplicationRecord
 
     has_many :wines
     has_many :users, through: :wines
+
+    def self.order_by_established
+        self.order(:established)
+    end
+
+    def self.order_by_name
+        self.order(:name)
+    end
+
+    def self.order_by_region
+        self.order(:region)
+    end
 end
