7961f0b3fdcfcbe96d10666598709c81e32c7ad1
Michelle Ranly <meranly8@gmail.com>
2021-02-21 10:43:53 -0600

    Added login and errors logic into users#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,14 @@ class UsersController < ApplicationController
     end
 
     def create
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to root_path
+        else
+            flash[:message] = @user.errors.full_messages.join("")
+            render :new
+        end
     end
 
     def show
