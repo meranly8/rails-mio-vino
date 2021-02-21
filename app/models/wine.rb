73e8e0786daf0f98f4aa434a49ce0feb96f5777d
Michelle Ranly <meranly8@gmail.com>
2021-02-21 14:21:08 -0600

    Added Wine class methods to order_by_year and order_by_price

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -7,4 +7,12 @@ class Wine < ApplicationRecord
 
     belongs_to :user
     belongs_to :winery
+
+    def self.order_by_year
+        self.order(year: :desc)
+    end
+
+    def self.order_by_price
+        self.order(:price)
+    end
 end
