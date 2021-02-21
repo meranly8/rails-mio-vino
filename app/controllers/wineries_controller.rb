318af6eb6dcad82603bb362733e0dcca5550903d
Michelle Ranly <meranly8@gmail.com>
2021-02-21 16:51:55 -0600

    Defined wineries#show with @winery

--- app/controllers/wineries_controller.rb
+++ app/controllers/wineries_controller.rb
@@ -2,4 +2,8 @@ class WineriesController < ApplicationController
     def index
         @wineries = Winery.all
     end
+
+    def show
+        @winery = Winery.find_by(id: params[:id])
+    end
 end
