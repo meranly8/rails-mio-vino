42759357c3d0b388d92ed9db2d941ae1b02812fa
Michelle Ranly <meranly8@gmail.com>
2021-02-21 10:36:44 -0600

    Drew root route to welcome#home

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'welcome#home'
+
   get '/signup', to: 'sessions#new'
   post '/signup', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
