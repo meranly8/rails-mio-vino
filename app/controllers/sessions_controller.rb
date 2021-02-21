b1444b15c8a14447ca9e54f5155bd232cf6a96fc
Michelle Ranly <meranly8@gmail.com>
2021-02-21 11:51:54 -0600

    Added authenticate logic to login user in sesssions#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      flash[:message] = "Invalid credentials. Please try again"
+      redirect_to login_path
+    end
   end
 
   def destroy
