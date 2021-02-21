042563605a04c0891c6eeb4f0ed0b8d433eba038
Michelle Ranly <meranly8@gmail.com>
2021-02-21 11:04:39 -0600

    Corrected routes for signing up user

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcome#home'
 
-  get '/signup', to: 'sessions#new'
-  post '/signup', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  get '/user/:id', to: 'users#show', as: '/user'
 end
