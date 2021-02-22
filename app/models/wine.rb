840021b390fc00441123720e95d79a7a0e8346a4
Michelle Ranly <meranly8@gmail.com>
2021-02-21 22:07:06 -0600

    Added wine presence validations onto one line

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,9 +1,6 @@
 class Wine < ApplicationRecord
-    validates :name, presence: true
-    validates :wine_type, presence: true
-    validates :price, presence: true
+    validates :name, :wine_type, :price, :year, presence: true
     validates :price, numericality: {greater_than: 0}
-    validates :year, presence: true
 
     belongs_to :user
     belongs_to :winery
