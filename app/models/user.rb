8d876debb2ea543423fa8e78951bc68f4c86793a
Michelle Ranly <meranly8@gmail.com>
2021-02-21 22:09:21 -0600

    Added dependent delete_all to user has_many :wines

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
     validates :name, :email, presence: true
     validates :email, uniqueness: true
 
-    has_many :wines
+    has_many :wines, dependent: :delete_all
     has_many :wineries, through: :wines
 end
