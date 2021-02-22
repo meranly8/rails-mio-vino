d86804eee8aa348509d6ccf647855fff48f2e182
Michelle Ranly <meranly8@gmail.com>
2021-02-21 22:07:33 -0600

    Added uesr presence validations onto one line

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
 
-    validates :name, presence: true
-    validates :email, presence: true
+    validates :name, :email, presence: true
     validates :email, uniqueness: true
 
     has_many :wines
