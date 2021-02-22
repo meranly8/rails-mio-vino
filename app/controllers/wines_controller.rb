39b8ab46957cae148809bd32283ffbc1e807f86c
Michelle Ranly <meranly8@gmail.com>
2021-02-21 18:14:30 -0600

    Defined wines#create with save logic

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -10,7 +10,18 @@ class WinesController < ApplicationController
     def new
         @wine = Wine.new
     end
-    
+
+    def create
+        @wine = Wine.new(wine_params)
+        if @wine.save
+            redirect_to wine_path(@wine)
+        else
+            flash[:message] = @wine.errors.full_messages.join("")
+            render :new
+        end
+
+    end
+
     private
         def wine_params
             params.require(:wine).permit(:name, :wine_type)
