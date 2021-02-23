ba88b557c5202f75a292e334ecdcbc146bd467cb
Michelle Ranly <meranly8@gmail.com>
2021-02-22 20:52:43 -0600

    Defined wineries#create with create logic and #winery_params

--- app/controllers/wineries_controller.rb
+++ app/controllers/wineries_controller.rb
@@ -10,4 +10,19 @@ class WineriesController < ApplicationController
     def new
         @winery = Winery.new
     end
+
+    def create
+        @winery = Winery.new(winery_params)
+        if @winery.save
+            redirect_to winery_path(@winery)
+        else
+            flash[:message] = @winery.errors.full_messages.join("")
+            render :new
+        end
+    end
+
+    private
+        def winery_params
+            params.require(:winery).permit(:name, :state, :country, :region, :established)
+        end
 end
