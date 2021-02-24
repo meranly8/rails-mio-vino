7a4980804a269e8d593f91ea4fda0807d6f7447e
Michelle Ranly <meranly8@gmail.com>
2021-02-24 02:46:07 -0600

    Updated sessions#create flash[:message] value

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to user_path(user)
     else
-      flash[:message] = "Invalid credentials. Please try again"
+      flash[:message] = "Invalid credentials"
       redirect_to login_path
     end
   end
