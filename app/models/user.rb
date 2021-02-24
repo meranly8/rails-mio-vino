b87cdd81c41e2b9d6d9520bb4153441066d253a1
Michelle Ranly <meranly8@gmail.com>
2021-02-23 23:22:53 -0600

    Defined .create_from_omniauth() with .find_or_create_by logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,12 @@ class User < ApplicationRecord
 
     has_many :wines, dependent: :delete_all
     has_many :wineries, through: :wines
+
+    def self.create_from_omniauth(auth)
+        User.find_or_create_by(uid: auth['uid'], provider: auth['provider']) do |u|
+            u.name = auth['info']['first_name']
+            u.email = auth['info']['email']
+            u.password = SecureRandom.hex(16)
+        end
+    end
 end
