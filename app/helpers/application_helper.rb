dae4d9d4435f6f8c7f3ad927f6872c69ec43d029
Michelle Ranly <meranly8@gmail.com>
2021-02-25 01:22:52 -0600

    Defined #nav_bar application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+    def nav_bar
+        if logged_in?
+            "Hi, #{current_user.name} | #{link_to "Wine List", wines_path} | #{link_to "Winery List", wineries_path} | #{link_to "Logout", logout_path}"
+        end
+    end
 end
