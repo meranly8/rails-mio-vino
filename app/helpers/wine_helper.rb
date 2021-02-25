7451f9553f63fd4ebce742627bc0f3dee6476b8b
Michelle Ranly <meranly8@gmail.com>
2021-02-25 02:37:54 -0600

    Defined #header_display(), #display_winery_fields() Wine helpers

--- app/helpers/wine_helper.rb
+++ app/helpers/wine_helper.rb
@@ -1,2 +1,17 @@
 module WineHelper
+    def header_display(wine)
+        if wine
+            tag.h1("All #{wine.name} Wines")
+        else
+            tag.h2("All Wines")
+        end
+    end
+
+    def display_winery_fields(winery, f)
+        if winery
+            f.hidden_field :winery_id #will send wine_id in via params without user seeing anything
+        else
+            render partial: 'winery_select', locals: {f: f}
+        end
+    end
 end
